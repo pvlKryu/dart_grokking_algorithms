321b20a58cd961b80fcde002563c2a3bbe260bf4
@kryuchkov_ps <kryuchkov_ps@msk.atb.su>
2024-07-09 19:46:05 +0300

    binary search algorithm + tests

--- test/grokking_agorithms_test.dart
+++ test/grokking_agorithms_test.dart
@@ -1,8 +1,9 @@
-import 'package:grokking_agorithms/grokking_agorithms.dart';
+import 'package:grokking_agorithms/algorithms/binary_search.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('binary search', () {
+    expect(binarySearch([1, 2, 3, 4, 5], 2), 'Your num is found!');
+    expect(binarySearch([1, 2, 3, 4, 5], 12), 'Your num is not found!');
   });
 }

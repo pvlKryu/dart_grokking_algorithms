041cdc5f658d282beb1bb96cf9b5008eb024f784
@kryuchkov_ps <kryuchkov_ps@msk.atb.su>
2024-07-09 12:56:53 +0300

    init commit

---
+++ bin/grokking_agorithms.dart
@@ -0,0 +1,5 @@
+import 'package:grokking_agorithms/grokking_agorithms.dart' as grokking_agorithms;
+
+void main(List<String> arguments) {
+  print('Hello world: ${grokking_agorithms.calculate()}!');
+}

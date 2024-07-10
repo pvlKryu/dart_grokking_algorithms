42cc69b8c67d80121f15b1b421486d65d9b366ec
@kryuchkov_ps <kryuchkov_ps@msk.atb.su>
2024-07-10 08:13:24 +0300

    selection sort

---
+++ lib/sorting/selection_sort.dart
@@ -0,0 +1,17 @@
+/// O(N^2)
+List<int> selectionSort(List<int> nonSortedArray) {
+  for (int i = 0; i < nonSortedArray.length - 1; i++) {
+    int minIndex = i;
+    for (int j = i + 1; j < nonSortedArray.length; j++) {
+      if (nonSortedArray[j] < nonSortedArray[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex != i) {
+      int tmp = nonSortedArray[minIndex];
+      nonSortedArray[minIndex] = nonSortedArray[i];
+      nonSortedArray[i] = tmp;
+    }
+  }
+  return nonSortedArray;
+}
--- test/grokking_agorithms_test.dart
+++ test/grokking_agorithms_test.dart
@@ -1,9 +1,13 @@
 import 'package:grokking_agorithms/algorithms/binary_search.dart';
+import 'package:grokking_agorithms/sorting/selection_sort.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('binary search', () {
     expect(binarySearch([1, 2, 3, 4, 5], 2), 'Your num is found!');
     expect(binarySearch([1, 2, 3, 4, 5], 12), 'Your num is not found!');
+  });
+    test('selection sort', () {
+    expect(selectionSort([4, 2, 8, 1, 5]), [1, 2, 4, 5, 8]);
   });
 }
